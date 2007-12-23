87b59936ca6ea0ff63ec5baedc97fbf3046558bf
Roberto Saccon <rsaccon@gmail.com>
2007-12-23 05:34:13 +0000

    bugfixes

--- src/erlycairo/erlycairo_sup.erl
+++ src/erlycairo/erlycairo_sup.erl
@@ -36,7 +36,7 @@
 -behaviour(supervisor).
 
 %% API
--export([start_link/1, upgrade/0]).
+-export([start_link/0, start_link/1, upgrade/0]).
 
 %% Supervisor callbacks
 -export([init/1]).
@@ -49,6 +49,15 @@
 %% @doc Starts the supervisor
 %% @end 
 %%--------------------------------------------------------------------
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+    
+    
+%%--------------------------------------------------------------------
+%% @spec start_link(Args::list()) -> {ok,Pid} | ignore | {error,Error}
+%% @doc Starts the supervisor
+%% @end 
+%%--------------------------------------------------------------------    
 start_link(Args) ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, Args).
 
